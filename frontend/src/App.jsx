13363f7d2109c14e90501c708b695a745510da91
Taniya Bansal <taniya1agarwal@gmail.com>
2024-09-10 19:25:20 +0530

    Hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl text-purple-500 font-bold underline">
-      Hello world
-    </h1>
+      <Hero/>
     </div>
   )
 }
