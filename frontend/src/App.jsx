38a55492e10ed24cdd4addbbb6bb77995ec3a430
dharmander yadav <dharmander832@gmail.com>
2024-09-10 22:32:58 +0000

    add footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
       <Hero/>
+
+      <Footer/>
     </div>
   )
 }
