953bb5171e8dc9383ecace88ba67d0d92921e863
disshhhaa-1312 <dishamohite13122@gmail.com>
2024-09-11 18:30:46 +0530

    Added header to the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
-
+import Header from './components/Header'
 const App = () => {
   return (
     <div>
+      <Header>
+
+      </Header>
       <Hero/>
 
       <Footer/>
