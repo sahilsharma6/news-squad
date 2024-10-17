1ef379746d2d14100eb32036f2e8fd8473916d77
Rahul Gupta <iamrahulgupta4u@gmail.com>
2024-10-17 20:19:19 +0530

    fixed minor issues

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,21 +1,21 @@
 const router = express.Router();
 
-import express from 'express';
-import categoryController from '../controllers/categoryController.js';
+import express from "express";
+import * as categoryController from "../controllers/categoryController.js";
 
 // Route to create a new category
-router.post('/', categoryController.createCategory);
+router.post("/", categoryController.createCategory);
 
 // Route to get all categories
-router.get('/', categoryController.getAllCategories);
+router.get("/", categoryController.getAllCategories);
 
 // Route to get a category by ID
-router.get('/:id', categoryController.getCategoryById);
+router.get("/:id", categoryController.getCategoryById);
 
 // Route to update a category
-router.put('/:id', categoryController.updateCategory);
+router.put("/:id", categoryController.updateCategory);
 
 // Route to delete a category
-router.delete('/:id', categoryController.deleteCategory);
+router.delete("/:id", categoryController.deleteCategory);
 
-module.exports = router;
+export default router;
