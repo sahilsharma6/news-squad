ae30346442a95e0a377c7ae5f29e1faa3cf2a92d
ChannabasavaH <channabasavahalemani49@gmail.com>
2024-10-18 19:17:29 +0530

    create -f: post specfic page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
+import PostHeader from "./components/postPageComponents/PostHeader";
+import PostContent from "./components/postPageComponents/PostContent";
+import PostFooter from './components/postPageComponents/PostFooter';
+
 const App = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <PostHeader />
+      <PostContent />
+      <PostFooter />
       <Footer />
     </div>
   );
---
+++ frontend/src/components/postPageComponents/PostHeader.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import authorImg from '../../assets/authorImg.webp'
+import { IoMdEye } from "react-icons/io";
+import { PiChats } from "react-icons/pi";
+import { BsFillShareFill, BsTwitterX } from "react-icons/bs";
+import { FaFacebookF, FaWhatsapp, FaPinterestP } from "react-icons/fa";
+import girls from '../../assets/girls1.jpg'
+
+const PostHeader = () => {
+
+    const [postData, setPostData] = useState('');
+
+    useEffect(() => {
+        fetch('./postData.json')
+            .then((response) => response.json())
+            .then((data) => setPostData(data))
+            .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+
+    if (!postData) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className='m-4 p-2 w-1/2 flex flex-col gap-6 justify-center items-start bg-white'>
+            <div className='w-16 h-6 bg-red-500 flex justify-center items-center'>
+                <p className='text-sm text-center text-white font-semibold p-4'>Fashion</p>
+            </div>
+            <div className='w-full flex flex-col gap-y-4'>
+                <h1 className='text-4xl font-semibold'>{postData.title}</h1>
+                <div className='flex my-2 p-4 justify-between items-center'>
+                    <div className='flex justify-center items-center gap-x-2'>
+                        <img src={authorImg} alt="authorImg" className='w-8 h-8 rounded-full' />
+                        <span className='text-gray-400'>By</span>
+                        <p className='text-black font-semibold'>{postData.author}</p>
+                        <p>{postData.publishedDate}</p>
+                    </div>
+                    <div className='flex justify-center items-center gap-x-2'>
+                        <span><IoMdEye /></span>
+                        <p>{postData.views}</p>
+                        <span><PiChats /></span>
+                        <p>{postData.comments}</p>
+                    </div>
+                </div>
+                <div className='flex gap-x-2'>
+                    <div className='w-52 h-12 border-2 border-dotted flex justify-evenly items-center'>
+                        <p><BsFillShareFill /></p>
+                        <p className='font-medium'>Share</p>
+                    </div>
+                    <div className='w-12 h-12 bg-blue-600 flex justify-center items-center rounded-md'>
+                        <FaFacebookF size={20} color='white' />
+                    </div>
+                    <div className='w-12 h-12 bg-sky-600 flex justify-center items-center rounded-md'>
+                        <BsTwitterX size={20} color='white' />
+                    </div>
+                    <div className='w-12 h-12 bg-red-600 flex justify-center items-center rounded-md'>
+                        <FaPinterestP size={20} color='white' />
+                    </div>
+                    <div className='w-12 h-12 bg-green-600 flex justify-center items-center rounded-md'>
+                        <FaWhatsapp size={20} color='white' />
+                    </div>
+                </div>
+                <img src={girls} alt="girls" />
+            </div>
+        </div>
+    )
+}
+
+export default PostHeader
