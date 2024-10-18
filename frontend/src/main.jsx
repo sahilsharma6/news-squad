9d40aee0a6b7bef4d55d483658f8876676aa58f3
Pranaymaurya <139427478+Pranaymaurya@users.noreply.github.com>
2024-10-18 07:26:38 +0530

    edit-f:made changes in routing in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider,Route} from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import ArticlePage from "./pages/ArticlePage.jsx";
 import Home from "./pages/Home.jsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <div>Error</div>,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "article/:id", element: <ArticlePage /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<App />} >
+    <Route path="/" element={ <Home />} />
+    </Route>
+))
+// createBrowserRouter(
+//   [
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <div>Error</div>,
+//     children: [
+//       { path: "/", element: <Home /> },
+//       { path: "article/:id", element: <ArticlePage /> },
+//     ],
+//   },
+// ]
+// );
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
+import NewsLayout from "../HomeC/Cotent1";
 
 const Home = () => {
-  return <Hero />;
+  return (
+    <>
+    <Hero />
+    <NewsLayout/>
+    </>
+  );
 };
 
 export default Home;
