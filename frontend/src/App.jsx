9e15f3e7f6f4000a7b7d70aab35a985c6a4be2cb
Pranaymaurya <139427478+Pranaymaurya@users.noreply.github.com>
2024-10-21 20:22:11 +0530

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import NewsLayout from "./components/DontMiss-Comp";
+import NewsLayout from "./components/DontMissComp";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
