9be20aa5a1a2a611e12f9a805ad2412bd3246cbe
M Sami <beboolean@gmail.com>
2024-11-17 06:20:33 +0530

    feat: Dashboard CRUD Backend API's completed

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,24 @@
-import path from "path";
-import cors from "cors";
-// import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
 import cookieParser from "cookie-parser";
-dotenv.config();
 import connectDB from "./config/db.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-const port = process.env.PORT || 5000;
-connectDB();
-const app = express();
 
+// Route imports
 import categoryRoutes from "./routes/categoryRoutes.js";
 import imageUploadRoute from "./routes/imageUpload.js";
 import postRoutes from "./routes/postRoutes.js";
 import authRoute from "./routes/authRoute.js";
 import userRoutes from "./routes/userRoutes.js";
 
+dotenv.config();
+const port = process.env.PORT || 5000;
+connectDB();
+
+const app = express();
+
+// Middlewares
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -24,31 +26,23 @@ app.use(
   })
 );
 
-// app.use(cors({
-//   origin: 'https://travelsquads.com',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// routes
+
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/upload-image", imageUploadRoute);
 app.use("/api", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use(notFound);
-app.use(errorHandler);
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
